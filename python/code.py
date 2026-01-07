6eee95573f3a4acd33113a41b215b1a30cd24837
Arun-KumarRavi <arunkumarravi0811@gmail.com>
2026-01-07 11:18:43 +0530

    Initial commit - Jenkins Python project

---
+++ python/code.py
@@ -0,0 +1,10 @@
+print("🚀 Jenkins Test App")
+print("Version: 1.0")
+print("Status: Initial commit from GitHub")
+
+def main():
+    print("Hello from Python!")
+    print("Jenkins successfully pulled the code 🎉")
+
+if __name__ == "__main__":
+    main()
